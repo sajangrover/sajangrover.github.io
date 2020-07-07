5405826b1ba5b0bf36c0c64a8706559a93013309
sajangrover <sajangrover379@gmail.com>
2020-07-07 22:22:50 +0530

    my todo react app with on github pages

---
+++ precache-manifest.e722433dd611b7a8b0ecf2c0960db5dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2dbd574fa5371adeb729c0569b31e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a060ed8057d12f69e014",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "633b3d1018f58e717dfc",
+    "url": "/static/js/2.d46d8f21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d46d8f21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a060ed8057d12f69e014",
+    "url": "/static/js/main.2d9dfadc.chunk.js"
+  },
+  {
+    "revision": "99fba9a25b14f664f01f",
+    "url": "/static/js/runtime-main.6508bb52.js"
+  }
+]);
